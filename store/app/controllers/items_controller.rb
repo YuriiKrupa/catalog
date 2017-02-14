752721c2527e13ad9ffb2a0bcd4a52d70512842d
Yurii.K <yurii.krupa.dev@gmail.com>
2017-02-14 23:51:26 +0000

    Add base catalog functionality (create, edit, delete), add photo attaching and some bootstrap styles

---
+++ store/app/controllers/items_controller.rb
@@ -0,0 +1,51 @@
+class ItemsController < ApplicationController
+
+	before_action :set_item, only: [ :show, :edit, :update, :destroy]
+
+	def index
+		@items = Item.all
+			end
+
+	def show
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = Item.new(item_params)
+		unless @item.save
+			render :new
+		else
+			redirect_to @item
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @item.update_attributes(item_params)
+			redirect_to @item
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@item.destroy
+		redirect_to items_path
+	end
+
+	private
+
+	def set_item
+		@item = Item.find(params[:id])	
+	end
+
+	def item_params
+		params.require(:item).permit(:title, :description, :price, :image)		
+	end
+
+end
